54b2e1143900c6d032bf983ccb22df2ed81e6c98
Jaya kumar <jayakv1995@gmail.com>
2023-11-04 20:26:41 +0530

    clean code, morgan & dotenv enabled

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+import 'dotenv/config';
 import express from 'express';
 import bodyParser from "body-parser";
 import axios from 'axios';
+import morgan from 'morgan';
+
 
 const app = express()
 const port = 3000
 
+app.use(morgan('short'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const baseURL='https://api.openweathermap.org/data/2.5/weather?appid=145f5de071e552fcf321dd0bd0d2b44a&units=metric';
+
+const baseURL='https://api.openweathermap.org/data/2.5/weather?appid='+process.env.API_KEY+'&units=metric';
 
 async function weatherInfo(country) {
 
@@ -19,12 +24,12 @@ async function weatherInfo(country) {
     return data.main.temp+"C"
   }
 
-  let cities=[{"paris":"13.27C"},{"tirupati":"25.57C"},{"bangalore":"25.8C"}];
+let cities=[{"paris":"13.27C"},{"tirupati":"25.57C"},{"bangalore":"25.8C"}];
 
 app.get('/', (req, res) => {
 
     res.render('index.ejs');
-})
+});
 
 app.post('/getWeather',async (req,res)=>{
     //splitting countries with , delimter and generating array
@@ -32,22 +37,19 @@ app.post('/getWeather',async (req,res)=>{
     console.log(countries)
     let data = await Promise.all(countries.map(async (country)=>{
             let out = await weatherInfo(country);
-            // data.push(out);
             let obj = {};
             obj[country] = out;
-            // console.log(obj);
-            // data.push(obj);
             console.log(obj);
             return JSON.stringify(obj);
     }));
 
-    // const data = weatherInfo(req.body.country)
     console.log(typeof(data));
     console.log(data)
     res.render('response.ejs',{
         cities: data
-    });})
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
